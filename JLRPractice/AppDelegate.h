f9c68dc1edf9ceb4fcd2d5f4e4fdb3e2b2164d3f
Pablo Marti <startpablo@gmail.com>
2015-11-03 22:15:13 -0600

    Initial Commit

---
+++ JLRPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JLRPractice
+//
+//  Created by Pablo on 11/3/15.
+//  Copyright © 2015 CRU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JLRPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JLRPractice
+//
+//  Created by Pablo on 11/3/15.
+//  Copyright © 2015 CRU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
